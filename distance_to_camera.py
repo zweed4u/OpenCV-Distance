eab48cf30b467f45dec2574b0b6ceafe67211837
Zachary Weeden <zdw7287@rit.edu>
2018-03-31 19:03:30 -0400

    executable, indentation and show edge detection

--- distance_to_camera.py
+++ distance_to_camera.py
@@ -1,24 +1,26 @@
+#!/usr/bin/python3
 # import the necessary packages
 import numpy as np
 import cv2
  
 def find_marker(image):
-	# convert the image to grayscale, blur it, and detect edges
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (5, 5), 0)
-	edged = cv2.Canny(gray, 35, 125)
- 	
-	# find the contours in the edged image and keep the largest one;
-	# we'll assume that this is our rectangle of paper in the image
-	(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-	c = max(cnts, key = cv2.contourArea)
+    # convert the image to grayscale, blur it, and detect edges
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 35, 125)
+    resize_edged = cv2.resize(edged, (500, 640))                    # Resize image
+    cv2.imshow('edged', resize_edged)
+    # find the contours in the edged image and keep the largest one;
+    # we'll assume that this is our rectangle of paper in the image
+    (_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    c = max(cnts, key = cv2.contourArea)
  
-	# compute the bounding box of the of the paper region and return it
-	return cv2.minAreaRect(c)
+    # compute the bounding box of the of the paper region and return it
+    return cv2.minAreaRect(c)
 
 def distance_to_camera(knownWidth, focalLength, perWidth):
-	# compute and return the distance from the maker to the camera
-	return (knownWidth * focalLength) / perWidth
+    # compute and return the distance from the maker to the camera
+    return (knownWidth * focalLength) / perWidth
 
 # initialize the known distance from the camera to the object, which
 # in this case is 12 inches
@@ -40,18 +42,18 @@ focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 
 # loop over the images
 for imagePath in IMAGE_PATHS:
-	# load the image, find the marker in the image, then compute the
-	# distance to the marker from the camera
-	image = cv2.imread(imagePath)
-	marker = find_marker(image)
-	inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+    # load the image, find the marker in the image, then compute the
+    # distance to the marker from the camera
+    image = cv2.imread(imagePath)
+    marker = find_marker(image)
+    inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
  
-	# draw a bounding box around the image and display it
-	box = np.int0(cv2.boxPoints(marker))
-	cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-	cv2.putText(image, "%.2fft" % (inches / 12),
-		(image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-		1.0, (0, 255, 0), 3)
-	image = cv2.resize(image, (500, 640))                    # Resize image
-	cv2.imshow("image", image)
-	cv2.waitKey(0)
+    # draw a bounding box around the image and display it
+    box = np.int0(cv2.boxPoints(marker))
+    cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+    cv2.putText(image, "%.2fft" % (inches / 12),
+        (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+        1.0, (0, 255, 0), 3)
+    resized_image = cv2.resize(image, (500, 640))                    # Resize image
+    cv2.imshow("image", resized_image)
+    cv2.waitKey(0)
